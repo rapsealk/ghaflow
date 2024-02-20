329d8803786a7b634b7f690879a244a422e3bcaa
Jeongseok Kang <piono623@naver.com>
2024-02-20 18:10:53 +0900

    feat(ci): Check if branch name starts with valid prefix (#6)

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
 import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { PullRequest, PullRequestEvent } from "@octokit/webhooks-types";
 
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
-  core.info("Hello, world!");
+  if (github.context.eventName === "pull_request") {
+    // https://docs.github.com/en/actions/using-workflows/events-that-trigger-workflows#pull_request
+    const pullRequestEvent = github.context.payload as PullRequestEvent;
+    if (pullRequestEvent.action === "opened") {
+      onPullRequestOpened(pullRequestEvent.pull_request);
+    }
+  }
+}
+
+function onPullRequestOpened(pullRequest: PullRequest): void {
+  if (pullRequest.base.ref === "main") {
+    const isHeadFeatureBranch = pullRequest.head.ref.startsWith("feature/");
+    const isHeadFixBranch = pullRequest.head.ref.startsWith("fix/");
+    if (!isHeadFeatureBranch && !isHeadFixBranch) {
+      core.setFailed("Branch name does not start with `feature/` or `fix/`.");
+    }
+  }
 }
